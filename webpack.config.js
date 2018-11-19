17c34aa47efccc0e33e69c569d3e0744c8e3f711
Cesar Martinez <cesar.martinez@globalgaming.com>
2018-11-19 11:05:29 +0100

    Handle images and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,24 @@ module.exports = {
     rules: [{
       test: /\.scss$/,
       use: [
-        // fallback to style-loader in development
         MiniCssExtractPlugin.loader,
-        "css-loader",
-        "sass-loader"
+        'css-loader',
+        'sass-loader'
       ]
-    }]
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [ 'file-loader' ]
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [ 'file-loader' ]
+    }
+  ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css"
+      filename: 'style.css'
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
